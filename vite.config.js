7e71519826e7ae04f2c271e3bedcd96aea20f2a1
Azees253 <mohamed61@gmail.com>
2025-02-20 18:26:23 +0530

    modified

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/vite",
   plugins: [react()],
+  build: {
+    outDir: "dist", // This directory will be deployed
+  },
 });
