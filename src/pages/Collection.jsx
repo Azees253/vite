fc1c275b6835d92643049005e43407c86ad760d2
Azees253 <mohamed61@gmail.com>
2025-02-22 18:46:52 +0530

    update

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -3,6 +3,12 @@ import { CiFacebook } from "react-icons/ci";
 import { BsTwitter } from "react-icons/bs";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import "./pagesStyles/Collection.css";
+import black from "../assets/images/black.jpg";
+import white from "../assets/images/white.jpg";
+import cottan from "../assets/images/cottan.jpg";
+import jeans from "../assets/images/jeans.jpg";
+import Tshirt from "../assets/images/T-shirt.jpg";
+import placer from "../assets/images/placer.jpg";
 
 export default function Collection() {
   function handleClick(e) {
@@ -34,45 +40,27 @@ export default function Collection() {
         </form>
         <div className="product" id="product">
           <div className="product-box">
-            <img
-              style={{ width: "200px", height: "200px" }}
-              src="https://media.istockphoto.com/id/590039970/photo/man-wearing-a-white-shirt-white-background.jpg?s=612x612&w=0&k=20&c=r15FPjb--n45lV9lWTeUXQspeC1OrX9Nk55GMt6jae8="
-            ></img>
+            <img style={{ width: "200px", height: "200px" }} src={white}></img>
             <p>White shirt</p>
           </div>
           <div className="product-box">
-            <img
-              style={{ width: "200px", height: "200px" }}
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAZ5yPYvJ-oRt3wrkf7lzmHhdyhxD_8_pi7g&s"
-            ></img>
+            <img style={{ width: "200px", height: "200px" }} src={black}></img>
             <p>Black shirt</p>
           </div>
           <div className="product-box">
-            <img
-              style={{ width: "200px", height: "200px" }}
-              src="https://media.istockphoto.com/id/2171108543/photo/black-and-white-t-shirts-hanging-on-wooden-hangers-3d-rendering.jpg?s=612x612&w=0&k=20&c=22Rt7B5mUNWlB5umA9kmQmqUkbDndTcnMeU5bfUDDD8="
-            ></img>
+            <img style={{ width: "200px", height: "200px" }} src={Tshirt}></img>
             <p>T -shirt</p>
           </div>
           <div className="product-box">
-            <img
-              style={{ width: "200px", height: "200px" }}
-              src="https://media.istockphoto.com/id/186870715/photo/blue-jeans.jpg?s=612x612&w=0&k=20&c=MInRLnEABmDYzvxjnEiFBu1V20OaB1zHmgzCqUV5hzk="
-            ></img>
+            <img style={{ width: "200px", height: "200px" }} src={jeans}></img>
             <p>Jeans</p>
           </div>
           <div className="product-box">
-            <img
-              style={{ width: "200px", height: "200px" }}
-              src="https://media.istockphoto.com/id/171265514/photo/blue-and-pink-elegant-button-down-shirts-hanging-on-hangers.jpg?s=612x612&w=0&k=20&c=moHBkOegEKT9iUeZUyeEVdy27ptjP24C901Qqp36dDg="
-            ></img>
+            <img style={{ width: "200px", height: "200px" }} src={cottan}></img>
             <p>Cotten-shirt</p>
           </div>
           <div className="product-box">
-            <img
-              style={{ width: "200px", height: "200px" }}
-              src="https://media.istockphoto.com/id/1239108954/photo/handsome-elegant-fashionable-man-in-dark-suit-dark-checks-jacket.jpg?s=612x612&w=0&k=20&c=Krhsb58PO93fXdfIew7St_1MmprVs3XQphOqKO7wPyo="
-            ></img>
+            <img style={{ width: "200px", height: "200px" }} src={placer}></img>
             <p>Placer</p>
           </div>
         </div>
